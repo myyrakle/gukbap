d2819c694b370aee42c09a2a1e611c1e512d140b
myyrakle <sssang97@naver.com>
2020-09-04 09:29:45 +0900

    0.2.6

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
 require("./src/Array");
 require("./src/String");
 require("./src/String.Encoding");
+require("./src/Date");
+require("./src/Object");
--- src/Object.js
+++ src/Object.js
@@ -1,4 +1,4 @@
-function deepClone(obj) {
+function _deepClone(obj) {
     if (obj === null || typeof obj !== "object") {
         return obj;
     }
@@ -6,28 +6,28 @@ function deepClone(obj) {
     const result = Array.isArray(obj) ? [] : {};
 
     for (let key of Object.keys(obj)) {
-        result[key] = deepClone(obj[key]);
+        result[key] = _deepClone(obj[key]);
     }
 
     return result;
 }
 
-function copy(aObject) {
-    if (!aObject) {
-        return aObject;
-    }
+// function copy(aObject) {
+//     if (!aObject) {
+//         return aObject;
+//     }
 
-    let v;
-    let bObject = Array.isArray(aObject) ? [] : {};
-    for (const k in aObject) {
-        v = aObject[k];
-        bObject[k] = typeof v === "object" ? copy(v) : v;
-    }
+//     let v;
+//     let bObject = Array.isArray(aObject) ? [] : {};
+//     for (const k in aObject) {
+//         v = aObject[k];
+//         bObject[k] = typeof v === "object" ? copy(v) : v;
+//     }
 
-    return bObject;
-}
+//     return bObject;
+// }
 
-Object.prototype.clone = function () {
+Object.prototype.deepClone = function () {
     return deepClone(this);
 };
 
