d145a002df71c45b3a759b1a2a4a996f33c03538
myyrakle <sssang97@naver.com>
2020-09-09 20:45:30 +0900

    to pull

--- test.js
+++ test.js
@@ -1,10 +1,6 @@
 require("./index");
 
-const foo = { s: 33 };
-const cl = foo.clone();
-cl.s++;
-console.log(foo); // { s: 33 }
-console.log(cl); // { s: 34 }
+
 
 const arr = [1, 2, 3, 4];
 const cl = Object.assign({}, arr);
